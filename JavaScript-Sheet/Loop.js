1062560604823e3a3b82e26dc332ac1c83e2df96
Fauzy <haliffauzy45@gmail.com>
2024-12-03 20:32:13 +0700

    first commit

---
+++ JavaScript-Sheet/Loop.js
@@ -0,0 +1,15 @@
+// Loop
+let nilai = 15;
+for ( let nilai = 1; nilai <= 10; nilai++) {
+    console.log(nilai) // Akan berhenti di angka 10 
+}
+
+let number = 15;
+for ( let number = 0; number < 15; number++) {
+    console.log(number) // Akan berhenti di angka 14
+}
+
+const ArrayString = ["apel", 5, "buah"]
+for ( let i = 0; i < ArrayString.length; i++) {
+    console.log(ArrayString[i]) // menghitung panjang data Array 
+}
